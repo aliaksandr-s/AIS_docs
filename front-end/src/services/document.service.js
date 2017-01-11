eaa120214bf6cee3890a3e03be525b24f248fd84
aliaksandr-s <s-a-n-9@mail.ru>
2017-01-11 11:30:39 +0300

    Refactor services

--- front-end/src/services/document.service.js
+++ front-end/src/services/document.service.js
@@ -5,35 +5,33 @@
         .module('aisApp')
         .service('documentService', documentService);
 
-        documentService.$inject = ['$http', 'Upload'];
+    documentService.$inject = ['$http', 'Upload'];
 
-        function documentService($http, Upload) {
-            var test = function () {
-                console.log('test')
-            }
+    function documentService($http, Upload) {
+        var service = this;
 
-            var uploadDocuments = function (files) {
-                if (files && files.length) {
-                    for (var i = 0; i < files.length; i++) {
-                        var file = files[i];
-                        if (!file.$error) {
-                            Upload.upload({
-                                url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
-                                data: {
-                                    email: "email",
-                                    file: file
-                                }
-                            }).then(function (resp) {
-                                console.log('uploaded')
-                            });
-                        }
+        service.test = function () {
+            console.log('test')
+        }
+
+        service.uploadDocuments = function (files) {
+            if (files && files.length) {
+                for (var i = 0; i < files.length; i++) {
+                    var file = files[i];
+                    if (!file.$error) {
+                        Upload.upload({
+                            url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
+                            data: {
+                                email: "email",
+                                file: file
+                            }
+                        }).then(function (resp) {
+                            console.log('uploaded')
+                        });
                     }
                 }
-            };
-
-            return {
-                test: test,
-                uploadDocuments: uploadDocuments
             }
-        }
+        };
+
+    }
 })();
