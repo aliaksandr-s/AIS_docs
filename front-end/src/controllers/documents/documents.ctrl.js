ef77b9f77e444c99c627772783e9f6fbd1e4c9c0
aliaksandr-s <s-a-n-9@mail.ru>
2017-01-27 18:13:15 +0300

    Add document search and filters

--- front-end/src/controllers/documents/documents.ctrl.js
+++ front-end/src/controllers/documents/documents.ctrl.js
@@ -10,6 +10,9 @@
     function documentsCtrl (documentService) {
         var vm = this;
 
+        vm.sortType     = 'ownerName'; // set the default sort type
+        vm.sortReverse  = false;
+
         documentService.getAllDocuments().then(function (res) {
             vm.allDocuments = res;
             console.log(vm.allDocuments)
