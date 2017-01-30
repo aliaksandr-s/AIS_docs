146a34b6da8218e44d2f5d27ea1f2eafbd79efe9
Your Name <you@example.commmm>
2017-01-30 15:50:32 +0300

    Fixes

--- front-end/src/controllers/userDocuments/userDocuments.ctrl.js
+++ front-end/src/controllers/userDocuments/userDocuments.ctrl.js
@@ -11,7 +11,15 @@
         var vm = this;
 
         vm.user = $stateParams.user;
-        vm.documents = $stateParams.user.docs;
+        // vm.documents = $stateParams.user.docs;
+        console.log(vm.user._id)
+
+        documentService.getUserDocuments($stateParams.user._id).then(function (res) {
+            vm.documents = res;
+            console.log(vm.documents)    
+        }).catch(function (err) {
+            console.log(err)
+        })
 
         vm.getDocument = function(userId, docName) {
             documentService.downloadDocument(userId, docName)
